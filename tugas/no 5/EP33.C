240f87f2b242175c48667532b0db435cadafed88
Arif Nur Ramadhan <91766087+arifnrrmdn@users.noreply.github.com>
2022-10-24 04:02:55 +0700

    Update EP33.C

--- tugas/no 5/EP33.C
+++ tugas/no 5/EP33.C
@@ -4,18 +4,23 @@
 #define Bulan 30
 void main()
 {
+ 
+ /*deklarasi*/
  int HK;
  int TAHUN, BULAN;
 
  clrscr();
 
+ /*input data*/
  printf("Menghitung Hari, Bulan, dan Tahun sebuah projek");
  printf("\n-----------------------------------------------");
  printf("\nMasukan hari kerja sebuah projek = "); scanf("%d",&HK);
 
+ /*proses data*/
  TAHUN = HK/Tahun;
  BULAN =  HK/Bulan;
 
+ /*output data*/
  printf("\nHari yang dikerjakan sebuah projek selama = %d ", HK);
  printf("\nBulan yang dikerjakan sebuah projek selama = %d ", BULAN);
  printf("\nTahun yang dikerjakan sebuah projek selama = %d ", TAHUN);
