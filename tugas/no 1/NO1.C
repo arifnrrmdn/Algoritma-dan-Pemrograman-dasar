4fb18ce4e1ca71abda3c56ae75da9f7b086a8659
Arif Nur Ramadhan <91766087+arifnrrmdn@users.noreply.github.com>
2022-10-26 20:59:08 +0700

    Update NO1.C

--- tugas/no 1/NO1.C
+++ tugas/no 1/NO1.C
@@ -11,39 +11,40 @@ main()
  printf("\n DAFTAR MENU MAKANAN \n");
  printf("==========================");
  printf("\nPilih no menu di bawah ini \n");
- printf("1. Ayam Bakar \n");
- printf("2. Ayam Serundeng \n");
- printf("3. Ikan Bakar \n");
- printf("4. Pecel Lele \n");
- printf("5. Nasi \n");
+ printf("1. Nasi Goreng Ati Ampela \n");
+ printf("2. Nasi Goreng Maut \n");
+ printf("3. Nasi Goreng Pete \n");
+ printf("4. Nasi Goreng Bakso \n");
+ printf("5. Nasi Goreng Sosis \n");
  printf("0. Selesai dan bayar\n\n");
+
  do
  {
   printf("Pilih : "); scanf("%i",&pilih);
   if (pilih==1)
   {
-   printf("Harga Ayam Bakar Rp. 13.000,- \n");
-     sum1 =+ 13000;
+   printf("Harga Nasi Goreng Ati Ampela Rp. 16.000,- \n");
+     sum1 =+ 16000;
   }
   else if (pilih==2)
   {
-   printf("Harga Ayam Serundeng Rp. 13.000,- \n");
-     sum2 =+ 13000;
+   printf("Harga Nasi Goreng Maut Rp. 16.000,- \n");
+     sum2 =+ 16000;
   }
   else if (pilih==3)
   {
-   printf("Harga Ikan Bakar Rp. 10.000,- \n");
-     sum3 =+ 10000;
+   printf("Harga Nasi Goreng Pete Rp. 18.000,- \n");
+     sum3 =+ 18000;
   }
   else if (pilih==4)
   {
-   printf("Harga Pecel Lele Rp. 7.000,- \n");
-     sum4 =+ 7000;
+   printf("Harga Nasi Goreng Bakso Rp. 15.000,- \n");
+     sum4 =+ 15000;
   }
   else if (pilih==5)
   {
-   printf("Harga Nasi Rp. 4.000,- \n");
-     sum5 =+ 4000;
+   printf("Harga Nasi Goreng Rp. 15.000,- \n");
+     sum5 =+ 15000;
   }
   else if (pilih==0)
   {
